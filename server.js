532b732bd2bae13fa634f80b1d4b7e9e60e50e89
Ahmed El-Sharkawy <ahmed.melsharkawy@gmail.com>
2017-01-30 04:22:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -40,10 +40,10 @@ require('./app/routes')(app); // configure our routes
 
 // start app ===============================================
 // startup our app at http://localhost:8080
-app.listen(port);               
+app.listen(port, function(){               
 
 // shoutout to the user                     
-console.log('Magic happens on port ' + port);
+console.log('Magic happens on port ' + port)});
 
 // expose app           
 exports = module.exports = app;                         
