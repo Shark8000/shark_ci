044d19f383bf7a7d1fb21802d72f60d19ec48ec6
Ahmed El-Sharkawy <ahmed.melsharkawy@gmail.com>
2017-01-30 04:28:22 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 // config/db.js
     module.exports = {
-        url : 'mongodb://<dbuser>:<dbpassword>@ds135689.mlab.com:35689/stencil-dev'
+        url : 'mongodb://<admin>:<admin>@ds135689.mlab.com:35689/stencil-dev'
     }
